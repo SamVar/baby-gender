0bb48412f77c4e2a776e499ba70c10fb5b1460a5
SamVar <49461311+SamVar@users.noreply.github.com>
2026-04-30 19:26:24 -0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/baby-gender/",
   server: {
     host: "::",
     port: 8080,
